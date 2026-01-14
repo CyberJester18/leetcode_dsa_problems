94db2908601d54accdf2efaaa2504a0b505b678a
CyberJester18 <hthaiphule@gmail.com>
2026-01-14 17:12:08 +0530

    added Palindrome NUmber.java

---
+++ Palindrome NUmber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean a=false;
+        int temp=x;
+        int rem=0,rev=0;
+        while(x>0){
+            rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+        }
+        return rev==temp;
+    }
+}
