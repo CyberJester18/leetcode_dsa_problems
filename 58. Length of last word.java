3788287d72f824abcfea956da6e0c0c55b77785f
CyberJester18 <hthaiphule@gmail.com>
2026-02-06 23:03:11 +0530

    Create 58. Length of last word.java

---
+++ 58. Length of last word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int len = 0;
+        int i = s.length() - 1;
+
+        // skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // count characters of last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+}
